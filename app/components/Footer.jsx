319c920ace0c5684d455050609a7a7a95979bc43
okike victor <okikevictorodinaka@gmail.com>
2025-09-12 12:52:26 -0700

    footer change

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -30,9 +30,6 @@ const Footer = ({ isDarkMode }) => {
         okikevictorodinaka@gmail.com
       </div>
       <div className="text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6">
-        <p>
-          © {new Date().getFullYear()} Victor Johnson💎. All rights reserved.
-        </p>
         <ul className="flex items-center gap-10 justify-center mt-4 sm:mt-0">
           <li>
             <Link target="_blank" href="https://github.com/Vickyjohnson2004">
@@ -81,6 +78,9 @@ const Footer = ({ isDarkMode }) => {
             </Link>
           </li>
         </ul>
+        <p>
+          © {new Date().getFullYear()} Victor Johnson💎. All rights reserved.
+        </p>
       </div>
     </div>
   );
